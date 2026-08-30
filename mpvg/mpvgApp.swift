c2f08f7cb96ca820adbaaa21877668ae73bb0626
Fabián Sanhueza <sanhuesoft@gmail.com>
2026-08-30 17:46:12 -0400

    Initial Commit

---
+++ mpvg/mpvgApp.swift
@@ -0,0 +1,17 @@
+//
+//  mpvgApp.swift
+//  mpvg
+//
+//  Created by Fabián Sanhueza on 30-08-26.
+//
+
+import SwiftUI
+
+@main
+struct mpvgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
